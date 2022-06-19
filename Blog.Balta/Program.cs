ed1cc1ec156cd0921a5478e02ae9f7ee7ef4f6c1
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-06-19 12:42:36 -0300

    CRUD

--- Blog.Balta/Program.cs
+++ Blog.Balta/Program.cs
@@ -4,9 +4,13 @@ using Microsoft.Data.SqlClient;
 
 const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Encrypt=false";
 
-ReadUser();
+ReadUsers();
+//ReadUser();
+//CreateUser();
+//UpdateUser();
+//DeleteUser();
 
-static void ReadUser()
+static void ReadUsers()
 {
     using (var connection = new SqlConnection(CONNECTION_STRING))
     {
@@ -15,7 +19,67 @@ static void ReadUser()
 
         foreach (var user in users)
         {
-            Console.WriteLine(user.Name);
+            Console.WriteLine(user.Name + " - " + user.Email);
         }
+    }
+}
+
+static void ReadUser()
+{
+    using (var connection = new SqlConnection(CONNECTION_STRING))
+    {
+        var user = connection.Get<User>(1);
+
+        Console.WriteLine(user.Name + " - " + user.Email);
+    }
+}
+
+
+static void CreateUser()
+{
+    var user = new User()
+    {
+        Bio = "Equipe balta.io",
+        Email = "hello@balta.io",
+        Image = "https://...",
+        Name = "Equipe balta.io",
+        PasswordHash = "HASH",
+        Slug = "equipe balta"
+    };
+
+    using (var connection = new SqlConnection(CONNECTION_STRING))
+    {
+        connection.Insert<User>(user);
+        Console.WriteLine("Cadastro Realizado com sucesso!");
+    }
+}
+
+static void UpdateUser()
+{
+    var user = new User()
+    {
+        Id = 3,
+        Bio = "Equipe | balta.io",
+        Email = "hello@balta.io",
+        Image = "https://...",
+        Name = "Equipe de suporte balta.io",
+        PasswordHash = "HASH",
+        Slug = "equipe balta"
+    };
+
+    using (var connection = new SqlConnection(CONNECTION_STRING))
+    {
+        connection.Update<User>(user);
+        Console.WriteLine("Atualização Realizado com sucesso!");
+    }
+}
+
+static void DeleteUser()
+{
+    using (var connection = new SqlConnection(CONNECTION_STRING))
+    {
+        var user = connection.Get<User>(3);
+        connection.Delete<User>(user);
+        Console.WriteLine("Exclusão Realizado com sucesso!");
     }
 }
