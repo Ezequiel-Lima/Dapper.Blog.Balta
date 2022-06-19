4fedde11d81e0516d9cb0bf8b6e68569a3f1e231
Ezequiel Lima <joao.lima94@fatec.sp.gov.br>
2022-06-19 18:30:09 -0300

    Add Pattern Repository

--- Blog.Balta/Program.cs
+++ Blog.Balta/Program.cs
@@ -1,85 +1,25 @@
 ﻿using Blog.Balta.Models;
+using Blog.Balta.Repositories;
 using Dapper.Contrib.Extensions;
 using Microsoft.Data.SqlClient;
 
 const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Encrypt=false";
 
-ReadUsers();
+var connection = new SqlConnection(CONNECTION_STRING);
+
+connection.Open();
+ReadUsers(connection);
+connection.Close();
 //ReadUser();
 //CreateUser();
 //UpdateUser();
 //DeleteUser();
 
-static void ReadUsers()
+static void ReadUsers(SqlConnection connection)
 {
-    using (var connection = new SqlConnection(CONNECTION_STRING))
-    {
-        //só com isso eu consigo consulta todos os meus usuarios
-        var users = connection.GetAll<User>();
+    var repository = new UserRepository(connection);
+    var users = repository.GetAll();
 
-        foreach (var user in users)
-        {
-            Console.WriteLine(user.Name + " - " + user.Email);
-        }
-    }
-}
-
-static void ReadUser()
-{
-    using (var connection = new SqlConnection(CONNECTION_STRING))
-    {
-        var user = connection.Get<User>(1);
-
-        Console.WriteLine(user.Name + " - " + user.Email);
-    }
-}
-
-
-static void CreateUser()
-{
-    var user = new User()
-    {
-        Bio = "Equipe balta.io",
-        Email = "hello@balta.io",
-        Image = "https://...",
-        Name = "Equipe balta.io",
-        PasswordHash = "HASH",
-        Slug = "equipe balta"
-    };
-
-    using (var connection = new SqlConnection(CONNECTION_STRING))
-    {
-        connection.Insert<User>(user);
-        Console.WriteLine("Cadastro Realizado com sucesso!");
-    }
-}
-
-static void UpdateUser()
-{
-    var user = new User()
-    {
-        Id = 3,
-        Bio = "Equipe | balta.io",
-        Email = "hello@balta.io",
-        Image = "https://...",
-        Name = "Equipe de suporte balta.io",
-        PasswordHash = "HASH",
-        Slug = "equipe balta"
-    };
-
-    using (var connection = new SqlConnection(CONNECTION_STRING))
-    {
-        connection.Update<User>(user);
-        Console.WriteLine("Atualização Realizado com sucesso!");
-    }
-}
-
-static void DeleteUser()
-{
-    using (var connection = new SqlConnection(CONNECTION_STRING))
-    {
-        var user = connection.Get<User>(3);
-        connection.Delete<User>(user);
-        Console.WriteLine("Exclusão Realizado com sucesso!");
-    }
+    foreach (var user in users)
+        Console.WriteLine($"{user.Name} - {user.Email}"); 
 }
---
+++ Blog.Balta/Repositories/RoleRepository.cs
@@ -0,0 +1,49 @@
+﻿using Blog.Balta.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog.Balta.Repositories
+{
+    public class RoleRepository
+    {
+        private readonly SqlConnection _connection;
+
+        //é um construtor passando nossa conexao
+        // as setinhas => equivale ao return
+        public RoleRepository(SqlConnection connection)
+            => _connection = connection;
+
+        public IEnumerable<Role> GetAll()
+            => _connection.GetAll<Role>();
+
+        public Role Get(int id)
+            => _connection.Get<Role>(id);
+
+        public void Create(Role role)
+        {
+            role.Id = 0;
+            _connection.Insert<Role>(role);
+        }
+
+        public void Update(Role role)
+        {
+            if (role.Id != 0)
+                _connection.Update<Role>(role);
+        }
+
+        public void Delete(Role role)
+        {
+            if (role.Id != 0)
+                _connection.Delete<Role>(role);
+        }
+
+        public void Delete(int id)
+        {
+            if (id != 0)
+                return;
+
+            var role = _connection.Get<Role>(id);
+            _connection.Delete<Role>(role);
+        }
+    }
+}
